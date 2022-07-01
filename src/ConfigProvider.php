0d48f3dc6de2aef0c71a8b4976814c4d3e1838d1
Roland Rusch <roland@rusch.lu>
2022-07-01 17:46:28 +0200

    #1 Initial commit

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -14,11 +14,24 @@ class ConfigProvider
     public function __invoke()
     {
         return [
+            'templates' => [
+                'layout' => 'layout::adminlte-default',
+                'paths' => [
+                    'layout' => [__DIR__ . "/../templates/layout"]
+//            This is defined in the module src/**/src/ConfigProvider.php
+//            'part' => [__DIR__ . '/../../src/Notused/template/part']
+                ],
+            ],
             'ruga' => [
                 'asset' => [
-                    'rugalib/ruga-asset-adminlte' => [
-                        'scripts' => [],
-                        'stylesheets' => [],
+                    'rugalib/ruga-layout-adminlte' => [
+                        'scripts' => ['js/adminlte.js', 'plugins/sparkline/sparkline.js'],
+                        'stylesheets' => ['css/adminlte.min.css'],
+                        'require' => [
+                            'rugalib/asset-jquery' => '^3.5',
+                            'rugalib/asset-jqueryui' => '^1.11',
+                            'rugalib/asset-bootstrap' => '^4.6',
+                        ],
                     ],
                 ],
             ],
